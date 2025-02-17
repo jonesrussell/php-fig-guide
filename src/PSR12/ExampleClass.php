8eaa811b554158ab5a4b5075100247e6df257a56
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-02-17 02:32:56 +0000

    docs: Improve ExampleClass documentation and code comments
    - Enhanced class and method documentation with more descriptive PHPDoc comments
    - Added detailed parameter descriptions
    - Included inline comments to explain property assignments and method behaviors
    - Maintained consistent documentation style for PSR-12 example class

--- src/PSR12/ExampleClass.php
+++ src/PSR12/ExampleClass.php
@@ -6,6 +6,8 @@ namespace JonesRussell\PhpFigGuide\PSR12;
 
 /**
  * ExampleClass demonstrates a properly formatted class according to PSR-12.
+ * This class represents a person with a name and age, providing methods
+ * to get and set these properties, as well as a method to introduce the person.
  */
 class ExampleClass
 {
@@ -18,64 +20,64 @@ class ExampleClass
     /**
      * Constructor to initialize the ExampleClass.
      *
-     * @param string $name
-     * @param int $age
+     * @param string $name The name of the person.
+     * @param int $age The age of the person.
      */
     public function __construct(string $name, int $age)
     {
-        $this->name = $name;
-        $this->age = $age;
+        $this->name = $name; // Set the name property
+        $this->age = $age;   // Set the age property
     }
 
     /**
      * Get the name of the person.
      *
-     * @return string
+     * @return string The name of the person.
      */
     public function getName(): string
     {
-        return $this->name;
+        return $this->name; // Return the name property
     }
 
     /**
      * Get the age of the person.
      *
-     * @return int
+     * @return int The age of the person.
      */
     public function getAge(): int
     {
-        return $this->age;
+        return $this->age; // Return the age property
     }
 
     /**
      * Set the name of the person.
      *
-     * @param string $name
+     * @param string $name The new name of the person.
      * @return void
      */
     public function setName(string $name): void
     {
-        $this->name = $name;
+        $this->name = $name; // Update the name property
     }
 
     /**
      * Set the age of the person.
      *
-     * @param int $age
+     * @param int $age The new age of the person.
      * @return void
      */
     public function setAge(int $age): void
     {
-        $this->age = $age;
+        $this->age = $age; // Update the age property
     }
 
     /**
      * Provide a brief introduction of the person.
      *
-     * @return string
+     * @return string A string introducing the person with their name and age.
      */
     public function introduce(): string
     {
-        return "Hello, my name is {$this->name} and I am {$this->age} years old.";
+        return "Hello, my name is {$this->name} and I am {$this->age} years old."; // Return the introduction string
     }
 } 
